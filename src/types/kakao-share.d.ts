4689ecbe25dc500edd02cef0911640d162022145
soykeepgoing <soykeepgoing@gmail.com>
2026-02-01 16:25:32 +0900

    ✨ feat: 그룹 초대 버튼 및 기능 구현

--- src/types/kakao-share.d.ts
+++ src/types/kakao-share.d.ts
@@ -13,6 +13,28 @@ declare global {
 
   interface KakaoShare {
     sendDefault: (options: KakaoShareFeedOptions) => void;
+    createDefaultButton: (options: KakaoShareDefaultButtonOptions) => void;
+  }
+
+  interface KakaoShareDefaultButtonOptions {
+    container: string | HTMLElement;
+    objectType: "feed";
+    content: {
+      title: string;
+      description?: string;
+      imageUrl: string;
+      link: {
+        mobileWebUrl: string;
+        webUrl: string;
+      };
+    };
+    buttons?: Array<{
+      title: string;
+      link: {
+        mobileWebUrl: string;
+        webUrl: string;
+      };
+    }>;
   }
 
   interface KakaoShareFeedOptions {
