fbc4579c7edc12f3c4969582cfb936c9f6645a50
soykeepgoing <soykeepgoing@gmail.com>
2026-02-02 20:43:58 +0900

    ♻️ refactor: 공개 라우트 판별 로직을 프록시로 이동

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -10,18 +10,3 @@ export const ROUTES = {
   GROUPS: '/groups',
   GROUP_PREVIEW: '/groups/preview',
 } as const;
-
-export const PUBLIC_ROUTES = {
-  LOGIN: ROUTES.LOGIN,
-  LOGIN_TEST: ROUTES.LOGIN_TEST,
-  KAKAO: ROUTES.KAKAO_CALLBACK,
-  GROUP_PREVIEW: ROUTES.GROUP_PREVIEW,
-};
-
-const PUBLIC_ROUTE_VALUES = Object.values(PUBLIC_ROUTES);
-
-export function isPublicRoute(pathname: string): boolean {
-  return PUBLIC_ROUTE_VALUES.some(
-    (route) => pathname === route || pathname.startsWith(`${route}/`)
-  );
-}
