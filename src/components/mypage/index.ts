e02124f5ac00ef9785ca95368368564d030c19ae
soykeepgoing <soykeepgoing@gmail.com>
2026-02-05 11:24:29 +0900

    ✨ feat: 마이페이지 컴포넌트 추가 구현: 프로필 카드 및 메뉴 아이템

--- src/components/mypage/index.ts
+++ src/components/mypage/index.ts
@@ -1,3 +1,4 @@
 export { ProfileCard } from "./ProfileCard";
 export { ProfileCardSkeleton } from "./ProfileCardSkeleton";
 export { ProfileCardError } from "./ProfileCardError";
+export { MenuItem } from "./MenuItem";
