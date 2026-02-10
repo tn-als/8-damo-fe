6e12d2821c4377dd1f59ed990e89f34f4be6df19
soykeepgoing <soykeepgoing@gmail.com>
2026-02-10 16:39:06 +0900

    ♻️ refactor: 날짜 포맷 함수 모듈화

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatDateToMinute = (date: Date): string => {
+    const yyyy = date.getFullYear();
+    const MM = String(date.getMonth() + 1).padStart(2, "0");
+    const dd = String(date.getDate()).padStart(2, "0");
+    const HH = String(date.getHours()).padStart(2, "0");
+    const mm = String(date.getMinutes()).padStart(2, "0");
+
+    return `${yyyy}-${MM}-${dd} ${HH}:${mm}`;
+}
