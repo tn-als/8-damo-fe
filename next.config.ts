318ad4bb2e84e28dd794e87fc8e23ded14dd7d37
soykeepgoing <soykeepgoing@gmail.com>
2026-02-10 14:24:06 +0900

    🔨 chore: 센트리 설정

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,30 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import { withSentryConfig } from '@sentry/nextjs';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns:[
+    remotePatterns: [
       {
-        protocol: "https",
-        hostname: "d3susamqetme5j.cloudfront.net",
-        pathname: "/s3/images/**", 
-      }
-    ]
-  }
+        protocol: 'https',
+        hostname: 'd3susamqetme5j.cloudfront.net',
+        pathname: '/s3/images/**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: 'damo-dev',
+  project: 'damo-dev-fe',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: '/monitoring',
+
+  webpack: {
+    automaticVercelMonitors: true,
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
+});
