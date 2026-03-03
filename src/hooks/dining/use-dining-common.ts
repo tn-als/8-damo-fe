4d14155f93d757e9d43a19eb727b9ae88e9eb64e
soykeepgoing <soykeepgoing@gmail.com>
2026-03-03 10:38:49 +0900

    ♻️ refactor: 회식 공통 정보 refetch 주기 수정

--- src/hooks/dining/use-dining-common.ts
+++ src/hooks/dining/use-dining-common.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getDiningCommon } from "@/src/lib/api/client/dining";
 import type { DiningCommonResponse } from "@/src/types/api/dining";
 
-const POLLING_INTERVAL_MS = 300_000;
+const POLLING_INTERVAL_MS = 20_000;
 
 export function useDiningCommon(
   groupId: string,
