fe82c28f89eb92d88469b0524023365630626ca1
soykeepgoing <soykeepgoing@gmail.com>
2026-03-12 14:36:00 +0900

    ♻️ refactor: Sentry 클라이언트 초기화를 idle provider로 위임

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,6 +1,2 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
-});
+// Client Sentry initialization is deferred to SentryIdleProvider.
+export {};
